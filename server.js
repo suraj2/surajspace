f5353ce7126a167b461c9bda3beb1e8ad3392bb9
suraj.ck@rksv.in <suraj.ck@rksv.in>
2017-04-07 17:36:04 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname+'/client'));
+console.log(__dirname+'/client')
+var port = 3000;
+app.listen(port, function() {
+        console.log("Listening on " + port);
+});
