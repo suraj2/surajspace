4d6cc6126fb75812c9b1c549e3f4b000e59b6969
suraj.ck@rksv.in <suraj.ck@rksv.in>
2017-04-08 21:50:57 +0530

    Last meaningful test run

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname+'/client'));
+app.use(express.static(__dirname+'/../client'));
 console.log(__dirname+'/client')
 var port = 3000;
 app.listen(port, function() {
